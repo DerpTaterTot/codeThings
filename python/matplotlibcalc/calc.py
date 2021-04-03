5a00994749b278aac896bb6b2036449641c548a7
Albert Chen <chenalbert06@gmail.com>
2021-04-03 15:09:08 -0400

    basic graphing calculator functionality done

--- python/matplotlibcalc/calc.py
+++ python/matplotlibcalc/calc.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 print("welcome to a graphing calculator coded in python")
 
@@ -9,13 +10,32 @@ coefficients = [0] * (degree + 1)
 for i in range(degree + 1):
     coefficients[i] = int(input(f"enter {i + 1} coefficient: "))
 
+coefficients = coefficients[::-1]
+
 lowerx = int(input("enter lower bound of domain: "))
 upperx = int(input("enter upper bound of domain: "))
 
 lowery = int(input("enter lower bound of range: "))
 uppery = int(input("enter upper bound of range: "))
 
+x_list = np.arange(lowerx, upperx + 1, 0.1)
+y_list = []
+
+for x in x_list:
+    y_sum = 0
+    for i in range(1, len(coefficients) + 1):
+        coefficient = coefficients[i - 1]
+        if i == 1:
+            y_sum += coefficient
+        else:
+            y_sum += coefficient * x ** (i - 1)
+
+    y_list.append(y_sum)
+
 plt.xlim([lowerx, upperx])
 plt.ylim([lowery, uppery])
 
+plt.grid()
+plt.plot(x_list, y_list)
 plt.show()
+
